da775aa49a639bc5d1e45b3b98cb069a6745a4b1
jenkpl <jenkpl@gmail.com>
2021-02-26 12:54:14 -0800

    coinflip streak

---
+++ js/02-coinflip-streak.js
@@ -0,0 +1,12 @@
+//declare variable
+let coinFlip;
+//do while loop
+do {
+    coinFlip = Math.round(Math.random());
+    if (coinFlip === 0) {
+    console.log("Heads");
+    } else if(coinFlip === 1) {
+    console.log("Tails");
+    }
+} while
+    (coinFlip === 0);
