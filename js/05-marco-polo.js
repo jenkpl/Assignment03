dfe6aaca50b5d5558ca53a4b5d738e60675bbf93
jenkpl <jenkpl@gmail.com>
2021-03-01 19:11:33 -0800

    all done
    finally cracked triangle and MPolo!

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -1,12 +1,10 @@
 for (let i = 1; i<101 ; i++) {
-    let threes = (i / 3);  //For each iteration it will check if the current number is odd or even
-    let fives = ( i / 5);
-    if (Number.isInteger(threes)) {  // display a message in the console window. 
-    console.log(`${i} is div by three.MARCO`);
-    } else if (Number.isInteger(fives)) { 
-    console.log(`${i} is div by fivePOLO.`);
-    } else if 
-    ((Number.isInteger(fives)) && (Number.isInteger(threes))) { 
-        console.log(`${i} is div by BOTH.`);
-} 
+    if ((i % 3 === 0) && (i % 5 === 0)) {  
+        console.log(`Marco! Polo!`);
+} else if (i % 3 === 0) {
+        console.log(`Marco!`); 
+} else if (i % 5 === 0) {
+    console.log(`Polo!`);    
+} else console.log(`${i}`);
 }
+
